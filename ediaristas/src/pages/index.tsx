017335db343f46a8598760560355c246e1ce157a
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2021-10-12 09:57:54 -0300

    Add last components to Index page

--- ediaristas/src/pages/index.tsx
+++ ediaristas/src/pages/index.tsx
@@ -3,9 +3,10 @@ import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
 import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment';
 import UserInformation from 'ui/components/data-display/UserInformation/UserInformation';
 import TextFieldMask from 'ui/components/inputs/TextFieldMask/TextFieldMask';
-import { Button, Typography } from '@mui/material';
+import { Button, Typography, Container } from '@mui/material';
 import { 
   FormElementsContainer,
+  ProfissionalContainer,
   ProfissionalPaper
 } from 'ui/styles/pages/index.style';
 
@@ -18,31 +19,53 @@ const Home: NextPage = () => {
         subtitle={'Preencha seu endereço e veja todos os profissionais da sua localidade'}
       />
 
-      <FormElementsContainer>
-        <TextFieldMask 
-          mask={'99.999-999'}
-          label={'Digite seu CEP'} 
-          fullWidth
-          variant={'outlined'}
-        />
-        <Typography color={'error'}>CEP inválido</Typography>
-        <Button 
-          variant={'contained'} 
-          color={'secondary'} 
-          sx={{width: '220px'}}
-        >
-          Buscar
-        </Button>
-      </FormElementsContainer>
+      <Container>
+        <FormElementsContainer>
+          <TextFieldMask 
+            mask={'99.999-999'}
+            label={'Digite seu CEP'} 
+            fullWidth
+            variant={'outlined'}
+          />
+          <Typography color={'error'}>CEP inválido</Typography>
+          <Button 
+            variant={'contained'} 
+            color={'secondary'} 
+            sx={{width: '220px'}}
+          >
+            Buscar
+          </Button>
+        </FormElementsContainer>
 
-      <ProfissionalPaper>
-        <UserInformation 
-          name={'Akira Hanashito'}
-          picture={'https://github.com/hanashiro.png'}
-          rating={3}
-          description={'São Paulo'}
-        />
-      </ProfissionalPaper>
+        <ProfissionalPaper>
+          <ProfissionalContainer>
+            <UserInformation 
+              name={'Akira Hanashito'}
+              picture={'https://github.com/hanashiro.png'}
+              rating={3}
+              description={'São Paulo'}
+            />
+            <UserInformation 
+              name={'Akira Hanashito'}
+              picture={'https://github.com/hanashiro.png'}
+              rating={3}
+              description={'São Paulo'}
+            />
+            <UserInformation 
+              name={'Akira Hanashito'}
+              picture={'https://github.com/hanashiro.png'}
+              rating={3}
+              description={'São Paulo'}
+            />
+            <UserInformation 
+              name={'Akira Hanashito'}
+              picture={'https://github.com/hanashiro.png'}
+              rating={3}
+              description={'São Paulo'}
+            />
+          </ProfissionalContainer>
+        </ProfissionalPaper>
+      </Container>
     </div>
   );
 }
