b311561a0dcbdc4c28985960f2233b36421524fe
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2021-10-16 10:53:40 -0300

    Create delete and edit actions

--- e-diaristas/src/main/java/br/com/treinaweb/ediaristas/controllers/DiaristaController.java
+++ e-diaristas/src/main/java/br/com/treinaweb/ediaristas/controllers/DiaristaController.java
@@ -5,6 +5,7 @@ import br.com.treinaweb.ediaristas.repositories.DiaristaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -36,6 +37,29 @@ public class DiaristaController {
     @PostMapping("/cadastrar")
     public String cadastrar(Diarista diarista) {
         repository.save(diarista);
+
+        return "redirect:/admin/diaristas";
+    }
+
+    @GetMapping("/{id}/editar")
+    public ModelAndView editar(@PathVariable Long id) {
+        var modelAndView = new ModelAndView("admin/diaristas/form");
+
+        modelAndView.addObject("diarista", repository.getById(id));
+
+        return modelAndView;
+    }
+
+    @PostMapping("/{id}/editar")
+    public String editar(@PathVariable Long id, Diarista diarista) {
+        repository.save(diarista);
+
+        return "redirect:/admin/diaristas";
+    }
+
+    @GetMapping("/{id}/excluir")
+    public String excluir(@PathVariable Long id) {
+        repository.deleteById(id);
 
         return "redirect:/admin/diaristas";
     }
